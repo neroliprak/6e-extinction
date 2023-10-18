cb75c4473f8c0e70e106f9717a5008fee151dd02
SoleneJeannin <124252654+SoleneJeannin@users.noreply.github.com>
2023-10-18 16:01:06 +0200

    Version 11
    barres qui apparaissent et ajout de la structure html (aucun element de style)

--- script.js
+++ script.js
@@ -1,106 +1,83 @@
 // Pour charger un fichier JSON
-fetch("donnees.json")
+fetch("donnees.json") 
     .then((response) => {
-        return response.json(); // renvoie les données JSON
+        return response.json(); // renvoie les données JSON ()
     })
     .then((listeExtinction) => {
         console.log(listeExtinction);
-        let data = listeExtinction;
-        const yearToCount = {}; // Un objet pour stocker le comptage par année        
-        
+        const espece_par_annee = {}; // Un objet pour stocker le nombre d'espèce par année  
+
         listeExtinction.forEach((item) => {
-            const year = item.date;
-            const statut = item.statut;
-            console.log(statut);
+            let year = item.date;
+            let statut = item.statut;
 
            // Récupérer les valeurs des boutons radio
             let radio = document.getElementsByName('stade');
             let stat;
+            //Parcours les options
             for (let i = 0; i < radio.length; i++) {
+                //si un bouotn radio est coché
                 if (radio[i].checked) {
+                    //prendre la valeur de ce bouton radio
                     stat = radio[i].value;
                 }
             }
-            console.log("stat =", stat);
-
+            //si dans le json, le statut est égal a la valeur du bouton radio coché, alors on l'ajoute au espece_par_annee (si on coche VU par exefmple, ca prend que les VU)
             if (statut==stat) {
-                if (yearToCount[year]) {
-                    yearToCount[year]++;
-                } else {
-                    yearToCount[year] = 1;
+                //Si il y a une espèce VU dans l'année year...
+                if (espece_par_annee[year]) {
+                    //...alors on augmente de 1 le nombre d'espèce de cette année
+                    espece_par_annee[year]++;
+                }
+                else {
+                    espece_par_annee[year] = 1;
                 }
             }
-            //Si on coche la case "tout"
+            //Si on coche la case "tout" (la valeur est vide)
             if (stat == "") {
-                if (yearToCount[year]) {
-                    yearToCount[year]++;
-                } else {
-                    yearToCount[year] = 1;
+                //on prend toutes les espèce de toutes les années (pas seulement VU par exemple)
+                if (espece_par_annee[year]) {
+                    espece_par_annee[year]++;
+                } 
+                else {
+                    //Initialisation => égal à 1 parce qu'il y a forcément une espèce dans l'annee (sinon il n'y aurait pas l'année)
+                    espece_par_annee[year] = 1;
                 }
             }
         });
         
-        console.log(yearToCount)
+        console.log(espece_par_annee); // Affiche le nombre d'espèces par année
 
-        let svgWidth = 400; // Largeur de la zone SVG
-        let svgHeight = 200; // Hauteur de la zone SVG
-        let barreWidth = svgWidth / Object.keys(yearToCount).length;
+        //Converti espece_par_annee (objet) en tableau     
+        let tab_espece_annee = Object.keys(espece_par_annee).sort().map(annee=>({annee, valeur:espece_par_annee[annee]}))
 
-        // Sélectionner l'élément SVG existant
-        let svg = d3.select("svg");
+        console.log(tab_espece_annee)
 
-//MARCHE PAS
-        // Création des barres
-        svg
-            .selectAll(".histobarre")
-            .data(Object.entries(yearToCount))
-            .enter()
-            .append("rect")
-            .attr("class", "histobarre")
-            .attr("x", (d, i) => i * barreWidth)
-            .attr("y", (d) => svgHeight - d[1] * 2) //JE COMPRENDS PAS ICI CE QUI SE PASSE
-            .attr("width", barreWidth)
-            .attr("height", (d) => d[1] * 2) //POURQUOI CA CHANGE RIEN , x,)
-            .attr("fill", "#00aeff");
-//MARCHE
-    // Au survol une seule barre se colore
-    svg.selectAll(".histobarre")
-            .on("mouseenter", function (e, d) {
-                d3.selectAll(".histobarre").style("opacity", 0.5);
-                d3.select(this).style("opacity", 1);
+        //Créé les barres avec notre tableau d'espèces
+        barres(tab_espece_annee);
+        
+        //fonction de création des barres
+        function barres(tab) {
+            let largeur_barre = 390 / tab.length;
+            console.log(largeur_barre);
 
-                let ratio = 400 / (d[1] * 10);
-                d3.select("#barre_bleue").transition().attr("width", d[1] * ratio); //PAREIL POURQUOI CA CHANGE RIEN, LA ?
-            })
-            .on("mouseleave", function (e) {
-                d3.selectAll(".histobarre").style("opacity", 1);
-                d3.select("#barre_bleue").transition().attr("width", 0);
-            });
+            d3.select("#graph")
+                .selectAll("g")
+                .data(tab)
+                .join("g")
+                .attr("class", "histobarre")
+                .attr("transform", (d, i) => `translate(${largeur_barre * i + 10} ,0)`) //l'emplacement horizontalement dépend de la largeur des barres et de l'emplacement dans le tableau (donc i)
 
+                //on créé un groupe avant les rectangles, parce que dans un groupe il y aura deux barres
 
-
-        /*
-        //NOTRE VIEUX CODE => le cours
-
-        console.log(yearToCount); // Affiche le nombre d'espèces par année
-
-        let largeur_barre = 390 / yearToCount.length;
-        console.log(largeur_barre);
-
-        d3.select("#graph")
-            .selectAll("g")
-            .data(yearToCount)
-            .join("g")
-            .attr("class", "histobarre")
-            .attr("transform", (d, i) => `translate(${largeur_barre * i} ,0)`) //l'emplacement horizontalement dépend de la largeur des barres et de l'emplacement dans le tableau (donc i)
-
-            //on créé un groupe avant les rectangles, parce que dans un groupe il y aura deux barres
-
-        d3.selectAll(".histobarre")
-            .append("rect")
-            .attr("fill", "#00aeff")
-            .attr("width", largeur_barre)
-            .attr("height", (d,i) => d.A ) //objet = d / numéro positionnnement = i
+            d3.selectAll(".histobarre")
+                .append("rect")
+                .attr("fill", "#00aeff")
+                .attr("width", largeur_barre-10)
+                .attr("height", (d,i) => d.annee-1940 ) //objet = d / numéro positionnnement = i
+                .attr("transform", `scale(1, -1)`) //met les barres au dessus de la ligne des abscisses 
+                
+               
 
             d3.selectAll(".histobarre") //sur toutes les barres
                 .on("mouseenter", function(e, d){ //au survol de la souris + mettre d en paramètre pour les barres verticales, d = données associaées à this
@@ -110,10 +87,10 @@ fetch("donnees.json")
                         .style("opacity", 1) //devient opaque
 
                     
-                    let ratio = 400 / (d.A + d.B) //produit en croix pour que les deux barres se touchent
+                    let ratio = 400 / (d.annee + d.valeur) //produit en croix pour que les deux barres se touchent
                     d3.select("#barre_bleue")
                         .transition() // mettre avant les attributs
-                        .attr("width", d.A * ratio)
+                        .attr("width", d.annee * ratio)
                 })
                 .on("mouseleave", function(e){ //quand on ne survol plus par la souris
                     d3.selectAll(".histobarre")
@@ -125,6 +102,6 @@ fetch("donnees.json")
                         .transition()
                         .attr("width", 0)
                 })
-                */
+        };
 
     })
